8fe5f093fbf9d7f9e435815045e4b20ed83ecbf9
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2024-11-10 16:02:12 +0200

    Tehtävä 3.10

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 morgan.token('json', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
