419a3abcec10683c9c317979f6b447a06205d947
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2024-11-10 15:50:17 +0200

    Tehtävä 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 morgan.token('json', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
+app.use(cors())
 
 let persons = [
     {
@@ -86,6 +88,6 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
